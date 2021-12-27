f90f9cfa6f592820cb7971a29b73dfcff6f194e0
alejandrosb8 <alejandrosanchezb08@gmail.com>
2021-12-27 16:31:30 -0400

    basic functions completed

--- app.js
+++ app.js
@@ -1,21 +1,42 @@
-const container = document.querySelector('.grid-container')
+const container = document.querySelector('.grid-container');
+var cells = document.querySelectorAll('.cell');
+btnClear = document.querySelector('#clearbtn');
+makeGrid(16);
+
+btnClear.addEventListener('click', () => {
+    cells.forEach(cell =>{
+        cell.classList.remove("colored");
+    })
+    reset();
+})
 
 function makeGrid(size){
+    cells = document.querySelectorAll('.cell')
+    cells.forEach(cell =>{
+        cell.remove();
+    })
+
     container.setAttribute('style', 'grid-template-rows: repeat(' + size +', 1fr); grid-template-columns: repeat(' + size +', 1fr);')
     for (let i = 0; i < size; i++){
         for (let j = 0; j < size; j++){
-            let cell = document.createElement('div')
-            container.appendChild(cell).className = 'cell'
+            let cell = document.createElement('div');
+            container.appendChild(cell).className = 'cell';
         }
     }
+    cells = document.querySelectorAll('.cell');
+
+    cells.forEach(cell => {
+        cell.addEventListener('mouseenter', (e) => {
+            e.target.classList.add("colored");
+        });
+    });
 }
 
-makeGrid(16)
-
-cells = document.querySelectorAll('.cell')
-
-cells.forEach(cell => {
-    cell.addEventListener('mouseenter', (e) => {
-        e.target.className = 'colored'
-    });
-});
+function reset(){
+    size = prompt('Set grid size (max 100)')
+    if (size > 100){
+        size = 100;
+    }
+    makeGrid(size)
+    cells = document.querySelectorAll('.cell')
+}
